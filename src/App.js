d731321b236ce042a1220de12a7dd176c15ff30e
Divya <divyakanabar9@gmail.com>
2023-08-19 19:14:03 -0700

    Edited another typo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
               <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">About Page</a>
+              <a className="nav-link" href="/">About</a>
             </li>
           </ul>
           <form className="d-flex" role="search">
