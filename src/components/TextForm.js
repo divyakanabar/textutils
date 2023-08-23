b7961c3f2fd923e342cfc1373c09e16a058d88d5
Divya <divyakanabar9@gmail.com>
2023-08-22 20:55:13 -0700

    Added length, lower case logic to TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,18 +6,33 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleLoClick = () =>{
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
   const handleOnChange = (event) =>{
     setText(event.target.value)
   }
 
   const [text, setText] = useState('Enter Text Here');
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <>
+        <div className="container">
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-    </div>
-  )
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    )
 }
