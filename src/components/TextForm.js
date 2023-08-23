f32c0dc2dfa81ce83d840e4b22e7b4d62c03ce07
Divya <divyakanabar9@gmail.com>
2023-08-22 21:10:23 -0700

    Added Clear Text button logic to TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,11 +11,15 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleClearClick = () =>{
+    setText('');
+  }
+
   const handleOnChange = (event) =>{
     setText(event.target.value)
   }
 
-  const [text, setText] = useState('Enter Text Here');
+  const [text, setText] = useState(' ');
   return (
         <>
         <div className="container">
@@ -25,9 +29,10 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text </button>
         </div>
         <div className="container my-3">
-            <h1>Your text summary</h1>
+            <h2>Your text summary</h2>
             <p>{text.split(" ").length} words, {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes read</p>
             <h2>Preview</h2>
