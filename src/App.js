bed0e75563514df23ea64433dabbbbf95773c379
Divya <divyakanabar9@gmail.com>
2023-08-26 17:30:00 -0700

    Created Alert.js to add auto dismissing alerts

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react';
-import About from './components/About';
+// import About from './components/About';
+import Alert from './components/Alert';
 
 
 function App() {
   // Checks whether dark mode is enabled or not
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+        setAlert(null);
+    }, 1500)
+  }
+
+
   const toggleMode = () =>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled!", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled!", "success");
     }
   }
   return (
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below" mode={mode} />
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
     </div>
     {/* <About /> */}
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,26 +4,31 @@ export default function TextForm(props) {
   const handleUpClick = () =>{
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UpperCase!", "success");
   }
 
   const handleLoClick = () =>{
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to LowerCase!", "success");
   }
 
   const handleCopy = () =>{
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to clipboard!", "success");
   }
 
   const handleExtraSpaces = () =>{
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert("Cleared extra spaced from the text!", "success");
   }
 
   const handleClearClick = () =>{
     setText('');
+    props.showAlert("The text has been cleared!", "success");
   }
 
   const handleOnChange = (event) =>{
